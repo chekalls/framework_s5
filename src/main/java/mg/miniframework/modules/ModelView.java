eb245466e14ee07fe46f2fa9c56ea2c33d47117e
chekalls <146121696+chekalls@users.noreply.github.com>
2025-11-07 15:52:30 +0300

    ajout classe ModelView

---
+++ src/main/java/mg/miniframework/modules/ModelView.java
@@ -0,0 +1,13 @@
+package mg.miniframework.modules;
+
+public class ModelView {
+    private String view;
+
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+}
