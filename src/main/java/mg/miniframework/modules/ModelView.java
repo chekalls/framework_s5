08c86ac3c5e84e7f7fc874b4e206bc1e45228f96
chekalls <146121696+chekalls@users.noreply.github.com>
2025-11-19 15:51:55 +0300

    ajout attribut data dans ModelView

--- src/main/java/mg/miniframework/modules/ModelView.java
+++ src/main/java/mg/miniframework/modules/ModelView.java
@@ -1,7 +1,27 @@
 package mg.miniframework.modules;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModelView {
     private String view;
+    private Map<String,Object> dataMap;
+
+    public ModelView(){
+        this.dataMap = new HashMap<>();
+    }
+
+    public void setData(String dataKey,Object objectValue){
+        dataMap.put(dataKey, objectValue);
+    }
+
+    public Map<String, Object> getDataMap() {
+        return dataMap;
+    }
+
+    public void setDataMap(Map<String, Object> dataMap) {
+        this.dataMap = dataMap;
+    }
 
     public String getView() {
         return view;
